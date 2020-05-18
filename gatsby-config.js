f54243eff73027e5744350144dd2d92be5466f22
Ron <roncham@gmail.com>
2020-05-18 14:14:49 -0700

    add sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,38 +1,42 @@
 //const accessToken = process.env.GOOGLE_SERVICE_ACCOUNT_CREDENTIALS;
 module.exports = {
-  plugins: [
-    'gatsby-plugin-resolve-src',
-    'gatsby-plugin-sass',
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/assets/images`
-      }
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-4041486-24",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "covid19.pegasusteam.com",
-        //cookieExpires: 0,
-      },
-    },
-    'gatsby-plugin-react-leaflet',
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp'
-    // 'gatsby-plugin-offline',
-  ]
+	siteMetadata: {
+		siteUrl: `https://covid19.pegasusteam.com`,
+	},
+	plugins: [
+		'gatsby-plugin-resolve-src',
+		'gatsby-plugin-sass',
+		'gatsby-plugin-react-helmet',
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/assets/images`,
+			},
+		},
+		{
+			resolve: `gatsby-plugin-google-analytics`,
+			options: {
+				trackingId: 'UA-4041486-24',
+				// Defines where to place the tracking script - `true` in the head and `false` in the body
+				head: true,
+				// Setting this parameter is optional
+				anonymize: true,
+				// Setting this parameter is also optional
+				respectDNT: true,
+				// Delays sending pageview hits on route update (in milliseconds)
+				pageTransitionDelay: 0,
+				// Any additional optional fields
+				sampleRate: 5,
+				siteSpeedSampleRate: 10,
+				cookieDomain: 'covid19.pegasusteam.com',
+				//cookieExpires: 0,
+			},
+		},
+		'gatsby-plugin-react-leaflet',
+		'gatsby-plugin-sharp',
+		'gatsby-transformer-sharp',
+		'gatsby-plugin-sitemap',
+		// 'gatsby-plugin-offline',
+	],
 };
