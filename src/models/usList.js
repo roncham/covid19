c8800ec31c01a962cb5f3ff009e1b52d95453705
Ron <roncham@gmail.com>
2020-05-26 16:59:21 -0700

    5 day avgs

--- src/models/usList.js
+++ src/models/usList.js
@@ -3,37 +3,37 @@ import React from 'react';
 import axiosbase from '../data/axiobase';
 
 class StatesList extends React.Component {
-  state = {
-    usData: [],
-  };
+	state = {
+		usData: [],
+	};
 
-  async componentDidMount() {
-    // Load async data.
-    const url = '/v2/states?sort=cases';
-    await axiosbase
-      .get(url)
-      .then((response) => response.data)
-      .then((data) => {
-        this.setState({ usData: data });
-        //console.log(this.state.usData);
-      });
-  }
+	async componentDidMount() {
+		// Load async data.
+		const url = '/v2/states?sort=cases';
+		await axiosbase
+			.get(url)
+			.then((response) => response.data)
+			.then((data) => {
+				this.setState({ usData: data });
+				//console.log(this.state.usData);
+			});
+	}
 
-  render() {
-    return (
-      <>
-        {this.state.usData.map((usData) => (
-          <tr key={usData.state}>
-            <th scope="row">{usData.state}</th>
-            <td className="is-yellow">{usData.cases}</td>
-            <td className="is-red">{usData.deaths}</td>
-            <td className="is-orange">{usData.active}</td>
-            <td className="is-blue">{usData.tests}</td>
-          </tr>
-        ))}
-      </>
-    );
-  }
+	render() {
+		return (
+			<>
+				{this.state.usData.map((usData) => (
+					<tr key={usData.state}>
+						<th scope="row">{usData.state}</th>
+						<td>{usData.cases}</td>
+						<td>{usData.deaths}</td>
+						<td>{usData.active}</td>
+						<td>{usData.tests}</td>
+					</tr>
+				))}
+			</>
+		);
+	}
 }
 
 export default StatesList;
